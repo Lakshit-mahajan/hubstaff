bfaba0eded9099ca733fcb6934824561e9313134
Lakshit-mahajan <lakshitmahajan4545@gmail.com>
2026-09-15 12:18:01 +0530

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -26,7 +26,7 @@ def browser_fixture(request):
         service = Service("C:/Users/ASUS/Downloads/geckodriver-v0.37.1-win64/geckodriver.exe")
         options = FirefoxOptions()
         options.add_argument("--start-maximized")
-        driver = webdriver.Firefox()
+        driver = webdriver.Firefox(options=options)
         driver.implicitly_wait(4)
 
     elif browser == "edge":
